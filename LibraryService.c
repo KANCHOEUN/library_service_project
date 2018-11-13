d5c10395f57e7ad2247df994a4853b4b7f65bad2
KANCHOEUN <kancho1216@naver.com>
2018-11-13 13:27:22 +0900

    getch()를 getchar()로 교체

--- LibraryService.c
+++ LibraryService.c
@@ -1,9 +1,6 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <unistd.h>
-#include <termio.h>
-
-int getch();
 
 void PrintMenu();
 void SignUp();
@@ -19,6 +16,8 @@ typedef struct client{
 
 int main()
 {
+  system("clear");
+
   while(1)
   {
     PrintMenu();
@@ -27,31 +26,15 @@ int main()
   return 0;
 }
 
-int getch(void){
-    int ch;
-    struct termios buf, save;
-    tcgetattr(0,&save);
-    buf = save;
-    buf.c_lflag &= ~(ICANON|ECHO);
-    buf.c_cc[VMIN] = 1;
-    buf.c_cc[VTIME] = 0;
-    tcsetattr(0, TCSAFLUSH, &buf);
-    ch = getchar();
-    tcsetattr(0, TCSAFLUSH, &save);
-    return ch;
-}
-
 void PrintMenu()
 {
   char num;
-
-  system("clear");
 
   printf(">>도서관 서비스<<\n");
   printf("1.회원 가입\t2.로그인\t3.프로그램 종료\n\n");
   printf("번호를 선택하세요 : ");
   scanf("%s", &num);
-  getch();
+  getchar();
 
   switch(num)
   {
@@ -65,7 +48,7 @@ void PrintMenu()
         printf("\n");
         exit(1);
     default :
-        printf("다시 입력해주세요.\n\n");
+        printf("\n다시 입력해주세요.\n\n");
         sleep(2);
         break;
   }
@@ -73,42 +56,45 @@ void PrintMenu()
 
 void SignUp()
 {
-        system("clear");
+  FILE *ofp; // ofp -> 데이터쓰기
 
-        FILE *ofp; // ofp -> 데이터쓰기
+  ofp = fopen("client.txt", "a"); // client.txt 파일 끝에 이어쓰기, 파일이 없을 시 만들기
 
-        ofp = fopen("client.txt", "a"); // client.txt 파일에 덮어쓰기, 파일이 없을 >시 만들기
+  client c1;
 
-        client c1;
+  printf("\n>> 회원 가입 <<\n");
+  printf("학번, 비밀번호, 이름, 주소, 전화번호를 입력하세요.\n\n");
 
-        printf(">> 회원 가입 <<\n\n");
+  printf("학번 : ");
+  scanf("%d", &c1.student_number);
+  getchar();
 
-        printf("학번 : ");
-        scanf("%d", &c1.student_number);
-        getch();
+  printf("비밀번호 : ");
+  scanf("%[^\n]", c1.password);
+  getchar();
 
-        printf("비밀번호 : ");
-        scanf("%[^\n]", c1.password);
-        getch();
+  printf("이름 : ");
+  scanf("%[^\n]", c1.name);
+  getchar();
 
-        printf("이름 : ");
-        scanf("%[^\n]", c1.name);
-        getch();
+  printf("주소 : ");
+  scanf("%[^\n]", c1.address);
+  getchar();
 
-        printf("주소 : ");
-        scanf("%[^\n]", c1.address);
-        getch();
+  printf("전화번호 : ");
+  scanf("%[^\n]", c1.phonenumber);
+  getchar();
 
-        printf("전화번호 : ");
-        scanf("%[^\n]", c1.phonenumber);
-        getch();
+  fprintf(ofp, "%d|%s|%s|%s|%s\n", c1.student_number, c1.password, c1.name, c1.address, c1.phonenumber);
 
-        fprintf(ofp, "%d|%s|%s|%s|%s\n", c1.student_number, c1.password, c1.name, c1.address, c1.phonenumber);
+  fclose(ofp);
 
-        fclose(ofp);
+  printf("\n회원가입이 되셨습니다.\n\n");
+  sleep(2);
+  system("clear");
 }
 
 void Login()
 {
-
+  // 학번으로 시작할 시에
 }
