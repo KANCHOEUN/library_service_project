22881de5b794da38d0d4b817272c61b2346785d5
Plzchocoemong <igun0423@naver.com>
2018-11-29 12:29:10 +0900

    Book.txt 변경사항 적용 / 구조체 받아서 정렬하는 함수 추가.

--- LibraryService.c
+++ LibraryService.c
@@ -23,338 +23,424 @@ void Search();
 
 union student_number
 {
-  int num;
-  char s[10];
+	int num;
+	char s[10];
 };
 
-typedef struct client{
-        union student_number id;
-        char password[20];
-        char name[20];
-        char address[100];
-        char phonenumber[30];
-        struct client * next;
+typedef struct client {
+	union student_number id;
+	char password[20];
+	char name[20];
+	char address[100];
+	char phonenumber[30];
+	struct client * next;
 }Client;
+
+typedef struct node {
+	long long isbn;
+	char name[50];
+	char publisher[50];
+	char author[50];
+	int booknum;
+	char library[50];
+	char yes_no;
+	struct node * next;
+}Book;
+
+Book * BookSort(Book *);
+Book * swap(Book *, Book *);
+Book * Bookbubblesort(Book *);
 
 int count = 1;
 
 int main()
 {
-  system("clear");
-  // client.txt 정렬 함수 따로 만들어서 넣기
+	system("clear");
+	// client.txt 정렬 함수 따로 만들어서 넣기
 
-  while(1)
-  {
-    PrintMenu();
-  }
+	while (1)
+	{
+		PrintMenu();
+	}
 
-  return 0;
+	return 0;
 }
 
 void PrintMenu()
 {
-  char num;
+	char num;
 
-  count = 1;
+	count = 1;
 
-  system("clear");
+	system("clear");
 
-  printf(">> 도서관 서비스 <<\n");
-  printf("1.회원 가입\t2.로그인\t3.프로그램 종료\n\n");
-  printf("번호를 선택하세요 : ");
-  scanf("%s", &num);
-  getchar();
+	printf(">> 도서관 서비스 <<\n");
+	printf("1.회원 가입\t2.로그인\t3.프로그램 종료\n\n");
+	printf("번호를 선택하세요 : ");
+	scanf("%s", &num);
+	getchar();
 
-  switch(num)
-  {
-    case '1' :
-        SignUp();
-        break;
-    case '2' :
-        Login();
-        break;
-    case '3' :
-        printf("\n");
-        exit(1);
-    default :
-        printf("\n다시 입력해주세요.\n\n");
-        sleep(2);
-        break;
-  }
+	switch (num)
+	{
+	case '1':
+		SignUp();
+		break;
+	case '2':
+		Login();
+		break;
+	case '3':
+		printf("\n");
+		exit(1);
+	default:
+		printf("\n다시 입력해주세요.\n\n");
+		sleep(2);
+		break;
+	}
 }
 
 void SignUp()
 {
-  // 회원가입에 추가해야할 사항
-  // 1. client.txt 파일 학번 순으로 정렬
-  // 2. client.txt 파일에 동일한 학번 발견 시 출력하고 다시 회원가입으로 돌아가기
-  FILE *ofp; // ofp -> 데이터쓰기
-  Client info;
+	// 회원가입에 추가해야할 사항
+	// 1. client.txt 파일 학번 순으로 정렬
+	// 2. client.txt 파일에 동일한 학번 발견 시 출력하고 다시 회원가입으로 돌아가기
+	FILE *ofp; // ofp -> 데이터쓰기
+	Client info;
 
-  ofp = fopen("client.txt", "a"); // client.txt 파일 끝에 이어쓰기, 파일이 없을 시 만들기
+	ofp = fopen("client.txt", "a"); // client.txt 파일 끝에 이어쓰기, 파일이 없을 시 만들기
 
-  printf("\n>> 회원 가입 <<\n");
-  printf("학번, 비밀번호, 이름, 주소, 전화번호를 입력하세요.\n\n");
+	printf("\n>> 회원 가입 <<\n");
+	printf("학번, 비밀번호, 이름, 주소, 전화번호를 입력하세요.\n\n");
 
-  printf("학번 : ");
-  scanf("%d", &(info.id.num));
-  getchar();
+	printf("학번 : ");
+	scanf("%d", &(info.id.num));
+	getchar();
 
-  printf("비밀번호 : ");
-  scanf("%[^\n]", info.password);
-  getchar();
+	printf("비밀번호 : ");
+	scanf("%[^\n]", info.password);
+	getchar();
 
-  printf("이름 : ");
-  scanf("%[^\n]", info.name);
-  getchar();
+	printf("이름 : ");
+	scanf("%[^\n]", info.name);
+	getchar();
 
-  printf("주소 : ");
-  scanf("%[^\n]", info.address);
-  getchar();
+	printf("주소 : ");
+	scanf("%[^\n]", info.address);
+	getchar();
 
-  printf("전화번호 : ");
-  scanf("%[^\n]", info.phonenumber);
-  getchar();
+	printf("전화번호 : ");
+	scanf("%[^\n]", info.phonenumber);
+	getchar();
 
-  fprintf(ofp, "%d|%s|%s|%s|%s\n", info.id.num, info.password, info.name, info.address, info.phonenumber);
+	fprintf(ofp, "%d|%s|%s|%s|%s\n", info.id.num, info.password, info.name, info.address, info.phonenumber);
 
-  fclose(ofp);
+	fclose(ofp);
 
-  printf("\n회원가입이 되셨습니다.\n\n");
-  sleep(2);
+	printf("\n회원가입이 되셨습니다.\n\n");
+	sleep(2);
 }
 
 int Login()
 {
-  FILE * ofp;
-  Client login;
-  Client * head = NULL;
-  Client * newNode = NULL;
-  Client * tail = NULL;
+	FILE * ofp;
+	Client login;
+	Client * head = NULL;
+	Client * newNode = NULL;
+	Client * tail = NULL;
 
-  char str1[10];
-  char str2[10];
+	char str1[10];
+	char str2[10];
 
-  ofp = fopen("client.txt", "rt");
+	ofp = fopen("client.txt", "rt");
 
-  printf("\n>> 로그인 <<\n");
-  printf("학번 : ");
-  scanf("%s", str1);
-  printf("비밀번호 : ");
-  scanf("%s", login.password);
+	printf("\n>> 로그인 <<\n");
+	printf("학번 : ");
+	scanf("%s", str1);
+	printf("비밀번호 : ");
+	scanf("%s", login.password);
 
-  newNode = (Client *)malloc(sizeof(Client));
+	while (!feof(ofp))
+	{
+		newNode = (Client *)malloc(sizeof(Client));
+		fscanf(ofp, "%[^|]|%[^|]|%*[^\n]", str2, newNode->password);
+		fgetc(ofp);
+		newNode->next = NULL;
 
-  while(!feof(ofp))
-  {
-    fscanf(ofp, "%[^|]|%[^|]|%*[^\n]", str2, newNode->password);
-    fgetc(ofp);
-    newNode->next = NULL;
+		if (strcmp(str2, "admin") == 0)
+		{
+			strcpy(newNode->id.s, str2);
+		}
+		else
+		{
+			newNode->id.num = atoi(str2);
+		}
 
-    if(strcmp(str2, "admin") == 0)
-    {
-      strcpy(newNode->id.s, str2);
-    }
-    else
-    {
-      newNode->id.num = atoi(str2);
-    }
+		if (head == NULL)
+		{
+			head = newNode;
+		}
+		else
+		{
+			tail->next = newNode;
+		}
 
-    if(head == NULL)
-    {
-      head = newNode;
-    }
-    else
-    {
-      tail->next = newNode;
-    }
+		tail = newNode;
 
-    tail = newNode;
+		if (strcmp(str1, "admin") == 0)
+		{
+			strcpy(login.id.s, str1);
+			if (strcmp(login.id.s, "admin") == 0)
+			{
+				if (strcmp(login.password, newNode->password) == 0)
+				{
+					while (count)
+					{
+						Admin_Login();
+					}
+					fclose(ofp);
+					return 0;
+				}
+			}
+			else
+			{
+				printf("아이디 혹은 비밀번호가 잘못되었습니다.\n\n");
+				return Login();
+			}
+		}
+		else
+		{
+			login.id.num = atoi(str1);
+			if (login.id.num == newNode->id.num)
+			{
+				if (strcmp(login.password, newNode->password) == 0)
+				{
+					while (count)
+					{
+						Client_Login();
+					}
+					fclose(ofp);
+					return 0;
+				}
+				else
+				{
+					printf("아이디 혹은 비밀번호가 잘못되었습니다.\n\n");
+					return Login();
+				}
+			}
+		}
+		if (feof(ofp))
+		{
+			printf("아이디 혹은 비밀번호가 잘못되었습니다.\n\n");
+			return Login();
+		}
+	}
+}
 
-    if(strcmp(str1, "admin") == 0)
-    {
-      strcpy(login.id.s, str1);
-      if(strcmp(login.id.s, "admin") == 0)
-      {
-        if(strcmp(login.password, newNode->password) == 0)
-        {
-          while(count)
-          {
-            Admin_Login();
-          }
-          fclose(ofp);
-          return 0;
-        }
-      }
-      else
-      {
-        printf("아이디 혹은 비밀번호가 잘못되었습니다.\n\n");
-        return Login();
-      }
-    }
-    else
-    {
-      login.id.num = atoi(str1);
-      if (login.id.num == newNode->id.num)
-      {
-        if(strcmp(login.password, newNode->password) == 0)
-        {
-          while(count)
-          {
-            Client_Login();
-          }
-          fclose(ofp);
-          return 0;
-        }
-        else
-        {
-          printf("아이디 혹은 비밀번호가 잘못되었습니다.\n\n");
-          return Login();
-        }
-      }
-    }
-    if(feof(ofp))
-    {
-      printf("아이디 혹은 비밀번호가 잘못되었습니다.\n\n");
-      return Login();
-    }
-  }
+Book * BookSort(Book * head) {
+	FILE * ifp;
+	head = NULL;
+	Book * newNode = NULL;
+	Book * tail = NULL;
+
+	ifp = fopen("book.txt", "rt");
+
+	while (!feof(ifp))
+	{
+		newNode = (Book *)malloc(sizeof(Book));
+		fscanf(ifp, "%lld|%[^|]|%[^|]|%[^|]|%d|%[^|]|%[^\n]", &(newNode->isbn), newNode->name, newNode->publisher, newNode->author, &(newNode->booknum), newNode->library, &(newNode->yes_no));
+		fgetc(ifp);
+		newNode->next = NULL;
+
+		if (head == NULL)
+		{
+			head = newNode;
+		}
+		else
+		{
+			tail->next = newNode;
+		}
+
+		tail = newNode;
+
+	}
+
+	fclose(ifp);
+	tail->next = NULL;
+
+	head = Bookbubblesort(head);
+
+	return head; //Book.txt 정보 구조체 헤드 리턴
+}
+
+Book * swap(Book * prev, Book * subs) {
+	prev->next = subs->next;
+	subs->next = prev;
+	return subs;
+}
+
+Book * Bookbubblesort(Book * head)
+{
+	Book *p, *q, *top;
+	int changed = 1;
+
+	top = (Book *)malloc(sizeof(Book));
+
+	top->next = head;
+
+	if (head != NULL && head->next != NULL) {
+		while (changed) {
+			changed = 0;
+			q = top;
+			p = top->next;
+			while (p->next != NULL) {
+				if (p->isbn > p->next->isbn) {
+					q->next = swap(p, p->next);
+					changed = 1;
+				}
+				q = p;
+				if (p->next != NULL)
+					p = p->next;
+			}
+		}
+	}
+	p = top->next;
+	free(top);
+	return p;
 }
 
 void Client_Login()
 {
-  system("clear");
+	system("clear");
 
-  char choice;
+	char choice;
 
-  printf(">> 회원 메뉴 <<\n");
-  printf("1. 도서 검색\t2. 내 대여 목록\n3. 개인정보 수정\t4. 회원 탈퇴\n5. 로그아웃\t6. 프로그램 종료\n\n");
-  printf("번호를 선택하세요 : ");
-  scanf("%s", &choice);
-  getchar();
+	printf(">> 회원 메뉴 <<\n");
+	printf("1. 도서 검색\t2. 내 대여 목록\n3. 개인정보 수정\t4. 회원 탈퇴\n5. 로그아웃\t6. 프로그램 종료\n\n");
+	printf("번호를 선택하세요 : ");
+	scanf("%s", &choice);
+	getchar();
 
-  switch(choice)
-  {
-    case '1' :
-        Search();
-        break;
-    case '2' :
-        My_Borrow_List();
-        break;
-    case '3' :
-        Edit();
-        break;
-    case '4' :
-        DropOut();
-        break;
-    case '5' :
-        count = 0;
-        break;
-    case '6' :
-        printf("\n");
-        exit(1);
-    default :
-        printf("\n다시 입력해주세요.\n\n");
-        sleep(2);
-        break;
-  }
+	switch (choice)
+	{
+	case '1':
+		Search();
+		break;
+	case '2':
+		My_Borrow_List();
+		break;
+	case '3':
+		Edit();
+		break;
+	case '4':
+		DropOut();
+		break;
+	case '5':
+		count = 0;
+		break;
+	case '6':
+		printf("\n");
+		exit(1);
+	default:
+		printf("\n다시 입력해주세요.\n\n");
+		sleep(2);
+		break;
+	}
 }
 
 void My_Borrow_List()
 {
-  printf("My_Borrow_List\n\n");
-  sleep(2);
+	printf("My_Borrow_List\n\n");
+	sleep(2);
 }
 
 void Edit()
 {
-  printf("Edit\n\n");
-  sleep(2);
+	printf("Edit\n\n");
+	sleep(2);
 }
 
 void DropOut()
 {
-  printf("DropOut\n\n");
-  sleep(2);
+	printf("DropOut\n\n");
+	sleep(2);
 }
 
 void Admin_Login()
 {
-  system("clear");
+	system("clear");
 
-  char choice;
+	char choice;
 
-  printf(">> 관리자 메뉴 <<\n");
-  printf("1. 도서 등록\t2. 도서 삭제\n3. 도서 대여\t4. 도서 반납\n5. 도서 검색\t6. 회원 목록\n7. 로그아웃\t8.프로그램 종료\n\n");
-  printf("번호를 선택하세요 : ");
-  scanf("%s", &choice);
-  getchar();
+	printf(">> 관리자 메뉴 <<\n");
+	printf("1. 도서 등록\t2. 도서 삭제\n3. 도서 대여\t4. 도서 반납\n5. 도서 검색\t6. 회원 목록\n7. 로그아웃\t8.프로그램 종료\n\n");
+	printf("번호를 선택하세요 : ");
+	scanf("%s", &choice);
+	getchar();
 
-  switch(choice)
-  {
-    case '1' :
-        Register();
-        break;
-    case '2' :
-        Delete();
-        break;
-    case '3' :
-        Borrow();
-        break;
-    case '4' :
-        Return();
-        break;
-    case '5' :
-        Search();
-        break;
-    case '6' :
-        List();
-        break;
-    case '7' :
-        count = 0;
-        break;
-    case '8' :
-        printf("\n");
-        exit(1);
-    default :
-        printf("\n다시 입력해주세요.\n\n");
-        sleep(2);
-        break;
-  }
+	switch (choice)
+	{
+	case '1':
+		Register();
+		break;
+	case '2':
+		Delete();
+		break;
+	case '3':
+		Borrow();
+		break;
+	case '4':
+		Return();
+		break;
+	case '5':
+		Search();
+		break;
+	case '6':
+		List();
+		break;
+	case '7':
+		count = 0;
+		break;
+	case '8':
+		printf("\n");
+		exit(1);
+	default:
+		printf("\n다시 입력해주세요.\n\n");
+		sleep(2);
+		break;
+	}
 }
 
 void Register()
 {
-        printf("Register\n\n");
-        sleep(2);
+	printf("Register\n\n");
+	sleep(2);
 }
 
 void Delete()
 {
-        printf("Delete\n\n");
-        sleep(2);
+	printf("Delete\n\n");
+	sleep(2);
 }
 
 void Borrow()
 {
-        printf("Borrow\n\n");
-        sleep(2);
+	printf("Borrow\n\n");
+	sleep(2);
 }
 
 void Return()
 {
-        printf("Return\n\n");
-        sleep(2);
+	printf("Return\n\n");
+	sleep(2);
 }
 
 void List()
 {
-        printf("List\n\n");
-        sleep(2);
+	printf("List\n\n");
+	sleep(2);
 }
 
 void Search()
 {
-        printf("Search\n\n");
-        sleep(2);
+	printf("Search\n\n");
+	sleep(2);
 }
