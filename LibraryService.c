defb8a7f91187c0b8a356295cb67c5f855b469b5
KANCHOEUN <kancho1216@naver.com>
2018-12-04 00:02:54 +0900

    Login() 수정, BookRead() 수정, Search() 함수 생성

--- LibraryService.c
+++ LibraryService.c
@@ -18,17 +18,8 @@ void Borrow();
 void Return();
 void List();
 
-void Search();
-// void BookList();
-
-union student_number
-{
-	int num;
-	char s[10];
-};
-
 typedef struct client {
-	union student_number id;
+	char id[10];
 	char password[20];
 	char name[20];
 	char address[100];
@@ -36,7 +27,7 @@ typedef struct client {
 	struct client * next;
 }Client;
 
-typedef struct node {
+typedef struct book {
 	long long isbn;
 	char name[50];
 	char publisher[50];
@@ -44,10 +35,17 @@ typedef struct node {
 	int booknum;
 	char library[50];
 	char yes_no;
-	struct node * next;
+	struct book * next;
 }Book;
 
-Book * BookSort(Book *);
+void Search();
+void NameFind(Book *); //책제목 검색
+void PublisherFind(Book *); //출판사 검색
+void IsbnFind(Book *); //isbn 검색
+void AuthorFind(Book *); //저자 검색
+void BookPrintList(Book *); //전체 검색(출력)
+
+Book * BookRead();
 Book * swap(Book *, Book *);
 Book * Bookbubblesort(Book *);
 
@@ -112,7 +110,7 @@ void SignUp()
 	printf("학번, 비밀번호, 이름, 주소, 전화번호를 입력하세요.\n\n");
 
 	printf("학번 : ");
-	scanf("%d", &(info.id.num));
+	scanf("%[^\n]", info.id);
 	getchar();
 
 	printf("비밀번호 : ");
@@ -131,7 +129,7 @@ void SignUp()
 	scanf("%[^\n]", info.phonenumber);
 	getchar();
 
-	fprintf(ofp, "%d|%s|%s|%s|%s\n", info.id.num, info.password, info.name, info.address, info.phonenumber);
+	fprintf(ofp, "%s|%s|%s|%s|%s\n", info.id, info.password, info.name, info.address, info.phonenumber);
 
 	fclose(ofp);
 
@@ -147,32 +145,20 @@ int Login()
 	Client * newNode = NULL;
 	Client * tail = NULL;
 
-	char str1[10];
-	char str2[10];
-
 	ofp = fopen("client.txt", "rt");
 
 	printf("\n>> 로그인 <<\n");
 	printf("학번 : ");
-	scanf("%s", str1);
+	scanf("%s", login.id);
 	printf("비밀번호 : ");
 	scanf("%s", login.password);
 
 	while (!feof(ofp))
 	{
 		newNode = (Client *)malloc(sizeof(Client));
-		fscanf(ofp, "%[^|]|%[^|]|%*[^\n]", str2, newNode->password);
+		fscanf(ofp, "%[^|]|%[^|]|%*[^\n]", newNode->id, newNode->password);
 		fgetc(ofp);
 		newNode->next = NULL;
-
-		if (strcmp(str2, "admin") == 0)
-		{
-			strcpy(newNode->id.s, str2);
-		}
-		else
-		{
-			newNode->id.num = atoi(str2);
-		}
 
 		if (head == NULL)
 		{
@@ -185,46 +171,39 @@ int Login()
 
 		tail = newNode;
 
-		if (strcmp(str1, "admin") == 0)
+		if (strcmp(login.id, "admin") == 0)
 		{
-			strcpy(login.id.s, str1);
-			if (strcmp(login.id.s, "admin") == 0)
+			if (strcmp(login.password, newNode->password) == 0)
 			{
-				if (strcmp(login.password, newNode->password) == 0)
+				while (count)
 				{
-					while (count)
-					{
-						Admin_Login();
-					}
-					fclose(ofp);
-					return 0;
+					Admin_Login();
 				}
+				fclose(ofp);
+				return 0;
+			}
+		}
+		else
+		{
+			printf("아이디 혹은 비밀번호가 잘못되었습니다.\n\n");
+			return Login();
+		}
+
+		if (strcmp(login.id, newNode->id) == 0)
+		{
+			if (strcmp(login.password, newNode->password) == 0)
+			{
+				while (count)
+				{
+					Client_Login();
+				}
+				fclose(ofp);
+				return 0;
 			}
 			else
 			{
 				printf("아이디 혹은 비밀번호가 잘못되었습니다.\n\n");
 				return Login();
-			}
-		}
-		else
-		{
-			login.id.num = atoi(str1);
-			if (login.id.num == newNode->id.num)
-			{
-				if (strcmp(login.password, newNode->password) == 0)
-				{
-					while (count)
-					{
-						Client_Login();
-					}
-					fclose(ofp);
-					return 0;
-				}
-				else
-				{
-					printf("아이디 혹은 비밀번호가 잘못되었습니다.\n\n");
-					return Login();
-				}
 			}
 		}
 		if (feof(ofp))
@@ -235,9 +214,9 @@ int Login()
 	}
 }
 
-Book * BookSort(Book * head) {
+Book * BookRead() {
 	FILE * ifp;
-	head = NULL;
+	Book * head = NULL;
 	Book * newNode = NULL;
 	Book * tail = NULL;
 
@@ -441,6 +420,185 @@ void List()
 
 void Search()
 {
-	printf("Search\n\n");
-	sleep(2);
+	 Book * head;
+	 head = BookRead();
+
+   char choice;
+
+   while(1)
+   {
+     printf(">> 도서 검색 <<\n");
+     printf("1. 도서명 검색\t2. 출판사 검색\n3. ISBN 검색\t4. 저자명 검색\n5.전체 검색\t6. 이전 메뉴\n\n");
+
+     printf("번호를 입력하세요 : ");
+     scanf("%s", &choice);
+     getchar();
+
+     switch(choice)
+     {
+       case 1 :
+          NameFind(head);
+          continue;
+       case 2 :
+          PublisherFind(head);
+          continue;
+       case 3 :
+          IsbnFind(head);
+          continue;
+       case 4 :
+          AuthorFind(head);
+          continue;
+       case 5 :
+          BookPrintList(head);
+          continue;
+       case 6 :
+          break;
+       default :
+          printf("\n다시 입력해주세요.\n\n");
+          sleep(2);
+          continue;
+     }
+     break;
+   }
+}
+
+void NameFind(Book * head) {
+   char search[50];
+   // int num = 0;
+   Book * tmp = head;
+   printf("도서명을 입력하세요 : ");
+   gets(search);
+   while (1) {
+      if (strstr(tmp->name, search) != NULL) {
+         //printf("%lld|%s|%s|%s|%d|%s|%c\n", tmp->isbn, tmp->name, tmp->publisher, tmp->author, tmp->booknum, tmp->library, tmp->yes_no);
+         printf(">> 검색 결과 <<\n");
+         printf("도서명 : %s\n", tmp->name);
+         printf("출판사 : %s\n", tmp->publisher);
+         printf("저자명 : %s\n", tmp->author);
+         printf("ISBN : %lld\n", tmp->isbn);
+         printf("소장처 : %s\n\n", tmp->library);
+         // 여기다가 대여 가능 목록 쓸 것
+         break;
+      }
+
+      if (tmp->next == NULL)
+      {
+        printf("존재하지 않는 도서입니다.\n\n");
+        break;
+      }
+      else
+      {
+        tmp = tmp -> next;
+      }
+   }
+}
+
+void PublisherFind(Book * head)
+{
+	char search[50];
+	// int num = 0;
+	Book * tmp = head;
+	printf("출판사를 입력하세요 : ");
+	gets(search);
+	while (1) {
+		 if (strstr(tmp->publisher, search) != NULL) {
+				//printf("%lld|%s|%s|%s|%d|%s|%c\n", tmp->isbn, tmp->name, tmp->publisher, tmp->author, tmp->booknum, tmp->library, tmp->yes_no);
+				printf(">> 검색 결과 <<\n");
+				printf("도서명 : %s\n", tmp->name);
+				printf("출판사 : %s\n", tmp->publisher);
+				printf("저자명 : %s\n", tmp->author);
+				printf("ISBN : %lld\n", tmp->isbn);
+				printf("소장처 : %s\n\n", tmp->library);
+				// 여기다가 대여 가능 목록 쓸 것
+				break;
+		 }
+
+		 if (tmp->next == NULL)
+		 {
+			 printf("존재하지 않는 도서입니다.\n\n");
+			 break;
+		 }
+		 else
+		 {
+			 tmp = tmp -> next;
+		 }
+	}
+}
+
+void IsbnFind(Book * head)
+{
+	long long search;
+	// int num = 0;
+	Book * tmp = head;
+	printf("ISBN을 입력하세요 : ");
+	scanf("%lld", &search);
+	getchar();
+	printf("\n");
+	while (1) {
+		 if (tmp->isbn == search) {
+				//printf("%lld|%s|%s|%s|%d|%s|%c\n", tmp->isbn, tmp->name, tmp->publisher, tmp->author, tmp->booknum, tmp->library, tmp->yes_no);
+				printf(">> 검색 결과 <<\n");
+				printf("도서명 : %s\n", tmp->name);
+				printf("출판사 : %s\n", tmp->publisher);
+				printf("저자명 : %s\n", tmp->author);
+				printf("ISBN : %lld\n", tmp->isbn);
+				printf("소장처 : %s\n\n", tmp->library);
+				// 여기다가 대여 가능 목록 쓸 것
+				break;
+		 }
+
+		 if (tmp->next == NULL)
+		 {
+			 printf("존재하지 않는 도서입니다.\n\n");
+			 break;
+		 }
+		 else
+		 {
+			 tmp = tmp -> next;
+		 }
+	}
+}
+
+void AuthorFind(Book * head)
+{
+	char search[50];
+	// int num = 0;
+	Book * tmp = head;
+	printf("저자명을 입력하세요 : ");
+	gets(search);
+	while (1) {
+		 if (strstr(tmp->author, search) != NULL) {
+				//printf("%lld|%s|%s|%s|%d|%s|%c\n", tmp->isbn, tmp->name, tmp->publisher, tmp->author, tmp->booknum, tmp->library, tmp->yes_no);
+				printf(">> 검색 결과 <<\n");
+				printf("도서명 : %s\n", tmp->name);
+				printf("출판사 : %s\n", tmp->publisher);
+				printf("저자명 : %s\n", tmp->author);
+				printf("ISBN : %lld\n", tmp->isbn);
+				printf("소장처 : %s\n\n", tmp->library);
+				// 여기다가 대여 가능 목록 쓸 것
+				break;
+		 }
+
+		 if (tmp->next == NULL)
+		 {
+			 printf("존재하지 않는 도서입니다.\n\n");
+			 break;
+		 }
+		 else
+		 {
+			 tmp = tmp -> next;
+		 }
+	}
+}
+
+void BookPrintList(Book * head)
+{
+	int i = 1; // 확인용 나중에 지워야 함
+	Book * tmp = head;
+	printf("\n");
+	while(tmp != NULL)
+	{
+		printf("%d. %lld|%s|%s|%s|%d|%s|%c\n", i++, tmp->isbn, tmp->name, tmp->publisher, tmp->author, tmp->booknum, tmp->library, tmp->yes_no);
+		tmp = tmp->next;
+	}
 }
